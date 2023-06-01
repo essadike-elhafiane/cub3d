2ba901e06a3353221ab2e61850fa9c8e5d3a13be
Essadike elhafiane <saddik.bo@gmail.com>
2023-06-01 16:50:35 +0100

    ls

---
+++ cube.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelhafia <eelhafia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 22:18:49 by eelhafia          #+#    #+#             */
+/*   Updated: 2023/06/01 15:25:02 by eelhafia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+#define CUBE_H
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+#include <mlx.h>
+#include "get_next_line/get_next_line.h"
+#include "libft/libft.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+
+int check_map(char **map);
+void check_line(char *map_line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelhafia <eelhafia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 22:17:53 by eelhafia          #+#    #+#             */
+/*   Updated: 2023/06/01 15:23:31 by eelhafia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube.h"
+
+// void mlx_mini_map(char **map)
+// {
+//    void *mlx;
+//    void *win;
+//    void *mini_im;
+   
+//    mlx = mlx_init();
+//    win = mlx_new_window(mlx , 640, 480 ,"minimap");
+//    mini_im =  mlx_new_image(mlx,50,50);
+   
+//     mlx_loop(mlx);    
+// }
+
+
+int main(int ac, char **av)
+{
+    int fd;
+    int len;
+    int i;
+    char *str;
+    char **map;
+
+    len = 0;
+    i = 0;
+    if (ac > 2)
+        return (write(1, "Error in arg \n", 14 ), 1);
+    fd = open(av[1], O_RDONLY, 0777);
+    if (fd < 0)
+        return (write(2, "Error open file\n", 16), 1);
+    while (1)
+    {
+        str = get_next_line(fd);
+        if (!str)
+            break;
+        free(str);
+        len++;
+    }
+    map = (char **) malloc(sizeof(char *) * (len + 1));
+    close(fd);
+    fd = open(av[1], O_RDONLY, 0777);
+    while (i <= len)
+        map[i++] = get_next_line(fd); 
+    if (check_map(map))
+        return (1);
+    // mlx_mini_map(map);
+    // i = 0;
+    // while (map[i])
+    //     printf("%s", map[i++]);
+    // printf("%s", map[i]);
+    
+}
