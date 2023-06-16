f6e35d66304ab6c3f1030dbdd86d30f390dffa05
Essadike elhafiane <saddik.bo@gmail.com>
2023-06-16 21:41:55 +0100

    Fri Jun 16 21:41:55 +01 2023

--- graphic/graph.c
+++ graphic/graph.c
@@ -6,7 +6,7 @@
 /*   By: eelhafia <eelhafia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 18:33:28 by eelhafia          #+#    #+#             */
-/*   Updated: 2023/06/16 20:33:10 by eelhafia         ###   ########.fr       */
+/*   Updated: 2023/06/16 21:38:32 by eelhafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,11 @@ void draw_pixel(int x, int y, unsigned int color)
 
 void draw_line(t_cub *data, double dis, double rotation)
 {
-	double end_x = data->plr->x_p + dis * cos(rotation);
-	double end_y = data->plr->y_p + dis * sin(rotation);
+	double end_x = data->plr->x_p + 32 + dis * cos(rotation);
+	double end_y = data->plr->y_p + 32 + dis * sin(rotation);
 
-	int x0 = data->plr->x_p;
-	int y0 = data->plr->y_p;
+	int x0 = data->plr->x_p + 32;
+	int y0 = data->plr->y_p + 32;
 	int x1 = (int)end_x;
 	int y1 = (int)end_y;
 	
@@ -85,6 +85,35 @@ void draw_line(t_cub *data, double dis, double rotation)
 		}
 	}	
 }
+
+int check_is_wall(double y, double x, t_cub *data)
+{
+	int idy  = y / 32;
+	int idx = x / 32;
+	int idy1  = (y + 1) / 32;
+	int idy_1  = (y - 1) / 32;
+	int idx1 = (x + 1) / 32;
+	if (idy >= data->hight_map)
+		return 1;
+	if (y < 0)
+		return (1);
+	if (x < 0)
+		return (1);
+	printf("x == %d | y == %d | c == %c \n", idx, idy, data->map[idy + 9][idx + 1]);
+	if (data->map[idy + 9][idx + 1] && (data->map[idy + 9][idx + 1] == '1' || data->map[idy + 9][idx1 + 1] == '1' \
+	|| data->map[idy1 + 9][idx + 1] == '1' || data->map[idy_1 + 9][idx + 1] == '1'))
+		return 1;
+	// if (!map[idy + 9][idx + 1] || map[idy + 9][idx + 1] == '1')
+	// 	return (1);
+	return (0);
+}
+
+
+double distance_p(double x, double y, double x1, double y1)
+{
+	return(sqrt(((x1-x) * (x1-x)) + ((y1-y) * (y1 - y))));
+}
+
 
 void frame_playr(void *f)
 {
@@ -113,29 +142,37 @@ void frame_playr(void *f)
 		}
 		i++;
 	}
-	draw_line(y,  10, y->plr->derction);
+	double r = M_PI / 180;
+	// distance
+	double x_dictance = y->plr->x_p;
+	double y_dictance = y->plr->y_p;
+	int m;
+
+	m = 0;
+	while (!check_is_wall(fabs(y_dictance), fabs(x_dictance), y))
+	{
+		x_dictance = (y->plr->x_p * sin(y->plr->derction)) + m;
+		y_dictance =( y->plr->y_p * cos(y->plr->derction)) + m;
+		m++;
+	}
+	double distance = distance_p(fabs(y->plr->x_p),fabs(y->plr->y_p), x_dictance, y_dictance);
+	if (distance > 0)
+		draw_line(y,  distance, y->plr->derction);
+	// draw_line(y,  100, y->plr->derction - r * 2);
+	// draw_line(y,  100, y->plr->derction - r);
+	// draw_line(y,  100, y->plr->derction + r);
+	// draw_line(y,  100, y->plr->derction + r * 2);
+	// draw_line(y,  100, y->plr->derction + r * 3);
+	// draw_line(y,  100, y->plr->derction + r * 4);
+	// draw_line(y,  100, y->plr->derction + r * 5);
+	// draw_line(y,  100, y->plr->derction + r * 6);
+	// draw_line(y,  100, y->plr->derction + r * 7);
+	// draw_line(y,  100, y->plr->derction + r * 8);
+	// draw_line(y,  100, y->plr->derction + r * 9);
+	// draw_line(y,  100, y->plr->derction + r * 10);
+	// draw_line(y,  100, y->plr->derction + r * 11);
 }
 
-int check_is_wall(char **map, int y, int x, t_cub *data)
-{
-	int idy  = y / 32;
-	int idx = x / 32;
-	int idy1  = (y + 1) / 32;
-	int idy_1  = (y - 1) / 32;
-	int idx1 = (x + 1) / 32;
-	if (idy >= data->hight_map)
-		return 1;
-	if (y < 0)
-		return (1);
-	if (x < 0)
-		return (1);
-	if (data->map[idy + 9][idx + 1] && (data->map[idy + 9][idx + 1] == '1' || data->map[idy + 9][idx1 + 1] == '1' \
-	|| data->map[idy1 + 9][idx + 1] == '1' || data->map[idy_1 + 9][idx + 1] == '1'))
-		return 1;
-	// if (!map[idy + 9][idx + 1] || map[idy + 9][idx + 1] == '1')
-	// 	return (1);
-	return (0);
-}
 
 void ft_hook1(void* param)
 {
@@ -145,7 +182,7 @@ void ft_hook1(void* param)
 		mlx_close_window(y->mlx);
 	if (mlx_is_key_down(y->mlx, MLX_KEY_UP))
 	{
-		if (!check_is_wall(y->map, y->plr->y_p + 2 * sin(y->plr->derction), y->plr->x_p + 2 * cos(y->plr->derction), y))
+		if (!check_is_wall(y->plr->y_p + 2 * sin(y->plr->derction), y->plr->x_p + 2 * cos(y->plr->derction), y))
 		{
 			printf("%f\n", y->plr->derction);
 			y->plr->y_p += 2 *sin(y->plr->derction);
@@ -156,7 +193,7 @@ void ft_hook1(void* param)
 	}
 	if (mlx_is_key_down(y->mlx, MLX_KEY_DOWN))
 	{
-		if (!check_is_wall(y->map, y->plr->y_p - 2 * sin(y->plr->derction), y->plr->x_p - 2 * cos(y->plr->derction), y))
+		if (!check_is_wall(y->plr->y_p - 2 * sin(y->plr->derction), y->plr->x_p - 2 * cos(y->plr->derction), y))
 		{
 			printf("%f\n", y->plr->derction);
 			y->plr->y_p -= 2 *sin(y->plr->derction);
